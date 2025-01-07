6d76d3da0c04a33348062081b3ea18d96dc9b6dd
Dominika <dominika_mazur99@wp.pl>
2025-01-07 09:23:24 +0100

    up

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL =
-    process.env.REACT_APP_API_URL || "http://localhost:3000/api";
-
 export const fetchData = async (url, method = "GET", data = null) => {
     const config = {
         method,
-        url: `${API_BASE_URL}${url}`,
+        url: `http://localhost:3000/api${url}`,
         data,
     };
 
